7363027d5d51192979320f3efa10bae2c3fc3b31
Filipe Fernandes <ocefpaf@gmail.com>
2019-10-08 15:33:32 -0400

    deprecate this library

--- ioos_qartod/__init__.py
+++ ioos_qartod/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '0.0.2'
+__version__ = '0.0.3'
+
+import warnings
+
+warnings.warn(
+    "The ioos_qartod is deprecated, please use https://github.com/ioos/ioos_qc",
+     DeprecationWarning,
+     stacklevel=2
+     )
